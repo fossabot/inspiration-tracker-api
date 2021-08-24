67fa7e93c0a786810456d119e47954badfad5f89
Ian Wright <ian@iantwright.com>
2021-08-24 18:14:14 -0400

    Added patch and delete to character router
    Allows for users to delete and update their existing characters.

--- src/routers/character.js
+++ src/routers/character.js
@@ -4,10 +4,6 @@ const auth = require('../middleware/auth');
 
 const router = new express.Router();
 
-router.get('/hello', (req, res) => {
-  res.send('helloCharacter');
-});
-
 router.post('/', auth, async (req, res) => {
   const character = new Character({
     ...req.body,
@@ -21,13 +17,63 @@ router.post('/', auth, async (req, res) => {
   }
 });
 
-router.get('/:name', auth, async (req, res) => {
+router.patch('/', auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'campaign'];
+  const isValid = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValid) {
+    return res.status(400).send({ error: 'Invalid update' });
+  }
+
   try {
-    const character = await Character.findOne({ name: new RegExp(`^${req.params.name}$`, 'i'), owner: req.user._id });
+    const character = await Character.findOne({
+      name: new RegExp(`^${req.query.name}$`, 'i'),
+      campaign: req.query.campaign,
+      owner: req.user._id
+    });
 
     if (!character) {
       return res.status(404).send();
     }
+
+    updates.forEach((update) => (character[update] = req.body[update]));
+    await character.save();
+    res.send(character);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const character = await Character.findOne({
+      name: new RegExp(`^${req.query.name}$`, 'i'),
+      campaign: req.query.campaign,
+      owner: req.user._id
+    });
+
+    if (!character) {
+      return res.status(404).send();
+    }
+    res.send(character);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    const character = await Character.findOneAndDelete({
+      name: new RegExp(`^${req.query.name}$`, 'i'),
+      campaign: req.query.campaign,
+      owner: req.user._id
+    });
+
+    if (!character) {
+      res.status(404).send();
+    }
+
     res.send(character);
   } catch (e) {
     res.status(500).send();
